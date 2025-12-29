6772344b00b1e42353b4f527eaf488d25369402e
mihirs <mihirsonar312@gmail.com>
2025-12-29 11:34:01 +0530

    update cors

--- Socket.js
+++ Socket.js
@@ -3,10 +3,13 @@ import { Server } from "socket.io";
 export const initSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
-      methods: ["GET", "POST"],
-    },
-  });
+  origin: [
+    "http://localhost:5173",
+    "bat-chit-frontend-4pqa.vercel.app"
+  ],
+  credentials: true
+ } 
+});
 
   io.on("connection", (socket) => {
     console.log("Client connected:", socket.id);
